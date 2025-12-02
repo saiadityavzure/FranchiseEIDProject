ba478c8a2e05bcaa677e82d107d27813ba948f7c
saiadityavzure <adiuru35@vzure.com>
2025-12-02 15:53:28 -0600

    Adding the ad services and the signals sync

---
+++ fims_directory_service/fims_core/ad/ad_sync_service.py
@@ -0,0 +1,48 @@
+from fims_core.ad.ldap_client import ADClient
+from fims_core.models import IdentityRegistry
+
+
+def sync_employee_to_ad(instance, employee_type: str):
+    """
+    Creates the AD user for CorpAdmin, LocAdmin, or LocationUser.
+    Stores DN + GUID + AD Status in IdentityRegistry.
+    """
+
+    # Get identity record
+    ident = IdentityRegistry.objects.filter(
+        employee_type=employee_type,
+        employee_ref_id=instance.id,
+    ).first()
+
+    if not ident:
+        return  # Identity not created? should never happen.
+
+    # Prevent re-creating users that already exist
+    if ident.ad_dn:
+        return
+
+    ad = ADClient()
+
+    if not ad.connect():
+        ident.ad_status = "ConnectionFailed"
+        ident.save()
+        return
+
+    try:
+        result = ad.create_user(
+            employee_type=employee_type,
+            eid=ident.eid,
+            first_name=instance.first_name,
+            last_name=instance.last_name,
+            email=instance.email or f"{ident.eid}@adtest.local",
+            password="TempPass@123",
+        )
+
+        ident.ad_dn = result["dn"]
+        ident.ad_guid = result["guid"]
+        ident.ad_status = "Created"
+        ident.save()
+
+    except Exception as e:
+        ident.ad_status = f"Failed: {str(e)}"
+        ident.save()
--- fims_directory_service/fims_core/signals.py
+++ fims_directory_service/fims_core/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from fims_core.identity import create_identity_for_employee
+from fims_core.ad.ad_sync_service import sync_employee_to_ad
 from fims_core.models import (
     CorpAdmin,
     LocAdmin,
@@ -11,33 +12,27 @@ from fims_core.models import (
 
 
 @receiver(post_save, sender=CorpAdmin)
-def create_identity_for_corp_admin(sender, instance: CorpAdmin, created: bool, **kwargs):
-    if not created:
-        return
-
-    create_identity_for_employee(
-        instance,
-        employee_type=IdentityRegistry.EmployeeType.CORP_ADMIN,
-    )
+def handle_corp_admin(sender, instance, created, **kwargs):
+    if created:
+        ident = create_identity_for_employee(
+            instance, IdentityRegistry.EmployeeType.CORP_ADMIN
+        )
+        sync_employee_to_ad(instance, IdentityRegistry.EmployeeType.CORP_ADMIN)
 
 
 @receiver(post_save, sender=LocAdmin)
-def create_identity_for_loc_admin(sender, instance: LocAdmin, created: bool, **kwargs):
-    if not created:
-        return
-
-    create_identity_for_employee(
-        instance,
-        employee_type=IdentityRegistry.EmployeeType.LOC_ADMIN,
-    )
+def handle_loc_admin(sender, instance, created, **kwargs):
+    if created:
+        ident = create_identity_for_employee(
+            instance, IdentityRegistry.EmployeeType.LOC_ADMIN
+        )
+        sync_employee_to_ad(instance, IdentityRegistry.EmployeeType.LOC_ADMIN)
 
 
 @receiver(post_save, sender=LocationUser)
-def create_identity_for_location_user(sender, instance: LocationUser, created: bool, **kwargs):
-    if not created:
-        return
-
-    create_identity_for_employee(
-        instance,
-        employee_type=IdentityRegistry.EmployeeType.LOCATION_USER,
-    )
+def handle_location_user(sender, instance, created, **kwargs):
+    if created:
+        ident = create_identity_for_employee(
+            instance, IdentityRegistry.EmployeeType.LOCATION_USER
+        )
+        sync_employee_to_ad(instance, IdentityRegistry.EmployeeType.LOCATION_USER)
